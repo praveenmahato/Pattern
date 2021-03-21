06e6468cbba70d072f0c9b007d99b8661138f259
Praveen Kumar Mahato <78393371+praveenmahato@users.noreply.github.com>
2021-03-21 23:17:14 +0530

    Create Pattern1.java

---
+++ Pattern1.java
@@ -0,0 +1,25 @@
+package com.company.pattern;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for(int i=1;i<=n ;i++){
+            for(int j= n;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+  /*  ******
+    ****
+    ***
+    **
+    **/
+}
